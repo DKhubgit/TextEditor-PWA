0c987fd8cb8ff9f4207aa8616ffa6d0f6f07477f
DanielK <Danielkang13@gmail.com>
2022-09-24 15:07:10 -0700

    added npm start command, add injectManifest to webpack config, updated service worker file to cache images.

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { request } = require('express');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -19,6 +20,7 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// cache strat, caches the routes for homepage and index.html
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
@@ -27,4 +29,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => request.destination === 'image', 
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({ // caches the image with status codes of 200 or 0
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, //days,hrs,min,sec
+      })
+    ]
+}));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file
 
 module.exports = () => {
   return {
@@ -18,12 +17,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({ //will create a index.html file in dists folder using template
+        title: 'Production html file',
+        template: './index.html',
+      }),
+      //add a injectManifest plugin for service worker
+
+      //add a manifest file (i beleive this is for installing the app)
     ],
 
     module: {
       rules: [
-        
+        //loaders preprocesses the file
+        { //loader for CSS
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        { //loader for JS
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
